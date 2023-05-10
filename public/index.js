35b81b481d6c1c04f84793a220ddde0c37b4f37d
IamLucifer0312 <trongtiennew@gmail.com>
2023-05-10 22:39:06 +0700

    Update UI

--- public/index.js
+++ public/index.js
@@ -14,7 +14,29 @@ document.querySelector("#send_message").addEventListener("click", async () => {
     const data = await response.json();
     console.log(data);
     createNewChatEntry(data.role, data.content);
+    
 });
+
+document.addEventListener("keydown", async () => {
+    if (event.key === "Enter") {
+        const user_message = document.querySelector("#user_message").value;
+        if (!user_message) return;
+        createNewChatEntry("user", user_message);
+        document.querySelector("#user_message").value = "";
+        const response = await fetch("/api/v1/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ role: "user", content: user_message }),
+        });
+        const data = await response.json();
+        console.log(data);
+        createNewChatEntry(data.role, data.content);
+    }
+
+});
+
 
 const createNewChatEntry = (role, content) => {
     const messages = document.querySelector("#messages");
@@ -32,3 +54,24 @@ const setScrollPosition = () => {
         messages.scrollTop = messages.scrollHeight;
     }
 }
+
+const closeChatWindow = () => {
+    let close = document.querySelector(".close");
+        let chat_window = document.querySelector(".wrapper");   
+        close.onclick = () =>{
+            chat_window.classList.remove("appear");
+            chat_window.classList.toggle("disappear");
+        }
+}
+
+const openChatWindow = () => {
+    let icon = document.querySelector(".icon");
+        let chat_window = document.querySelector(".wrapper");   
+        icon.onclick = () =>{
+            chat_window.classList.remove("disappear");
+            chat_window.classList.add("appear");
+        }
+}
+
+closeChatWindow();
+openChatWindow();
