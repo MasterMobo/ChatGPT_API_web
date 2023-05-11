5502c933fdb9bc19ea82f47f69983e617abc57cc
MasterMobo <mastermobo101@gmail.com>
2023-05-11 13:21:43 +0700

    added log and updated prompt

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const date = require("date-and-time");
 
 const chatRouter = require("./routes/chat");
 
@@ -11,5 +13,9 @@ app.use("/api/v1/chat", chatRouter);
 const PORT = process.env.PORT || 3000;
 const start = async () => {
     app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+    const now = date.format(new Date(), "DD/MM/YYYY HH:mm:ss");
+    fs.appendFileSync("chat.log", `[${now}] <<SESSION STARTED>>\n`, (err) => {
+        if (err) throw err;
+    });
 };
 start();
